0f7151452035656e7939c17ee7a1cfc40866227a
OliviaDemaret <oliviadtqb@gmail.com>
2022-11-16 16:15:30 +0100

    countryholiday-cli

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # countryholiday-CLI
+
+The tool will verify that the parameter is a well-formatted country name, transform it onto a two-letters country code, then perform an HTTP request to the API of the ![nager.date](https://date.nager.at/) service.
+
+It will show the results (a list of holidays dates for the current year) in a readable way in the terminal.
+
+## how it works
+
+Enter a country in the terminal like a parameter, then it will check if the given country exists. If the given country exists, it will use an API to display the lists of the holiday dates of the given country.
+
+For example:
+
+`@oliviademaret/holicountry Belgium`
+
+It will show:
+
+`2022-04-18 - Easter Monday - Easter Monday 2022-04-25 - Anzac Day - Anzac Day 2022-05-02 - May Day - May Day`
+
+## how to install it
+
+Write the following line in your terminal:
+
+`npm install @oliviademaret/holicountry`
---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+import axios from "axios";
+import { getCode, getNames } from "country-list";
+import chalk from "chalk";
+import ora from "ora";
+
+const country = process.argv[1];
+// const spinner = ora("Loading holiday date").start();
+
+getHoliday();
+
+// setTimeout(() => {
+//   spinner.color = "yellow";
+//   spinner.text = "Loading hol";
+// }, 3000);
+
+async function getHoliday() {
+  try {
+    const response = await axios.get(
+      `https://date.nager.at/api/v3/publicholidays/${getYear()}/${getCountryCode()}`
+    );
+
+    for (let i = 0; i < response.data.length; i++) {
+      let data = response.data[i];
+      console.log(`${data.date} - ${data.localName} - ${data.name}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function getYear() {
+  let years = new Date().getFullYear();
+  return years;
+}
+
+function getCountryCode() {
+  if (typeof getCode(country) == "undefined") {
+    console.log("error");
+  } else {
+    return getCode(country);
+  }
+}
